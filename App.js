f995f549420d20bb23eb779ec310e7ffc258051c
conoroshea1996 <conoshea12@gmail.com>
2019-10-22 15:53:01 +0100

    project setup

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Timer}>Hello</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EA4444',
+  },
+  Timer: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'white'
+  }
+});
